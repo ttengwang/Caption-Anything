36c8870874817876d167b8ebc19a3bc75dc6f759
zjr <zhangjr200012@outlook.com>
2023-04-08 17:46:01 +0800

    Implement git inference. Move base inference to blip

--- captioner/blip.py
+++ captioner/blip.py
@@ -8,16 +8,26 @@ import numpy as np
 from typing import Union
 
 
-from captioner.base_captioner import BaseCaptioner
+from base_captioner import BaseCaptioner
 
 class BLIPCaptioner(BaseCaptioner):
     def __init__(self, device):
         super().__init__(device)
         self.device = device
         self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
-        self.processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+        self.processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base", cache_dir=self.huggingface_cache_dir)
         self.model = BlipForConditionalGeneration.from_pretrained(
-            "Salesforce/blip-image-captioning-base", torch_dtype=self.torch_dtype).to(self.device)
+            "Salesforce/blip-image-captioning-base", torch_dtype=self.torch_dtype, cache_dir=self.huggingface_cache_dir).to(self.device)
+        
+
+    def inference(self, image: Union[np.ndarray, Image.Image, str]):
+        if type(image) == str: # input path
+                image = Image.open(image)
+        inputs = self.processor(image, return_tensors="pt").to(self.device, self.torch_dtype)
+        out = self.model.generate(**inputs, max_new_tokens=50)
+        captions = self.processor.decode(out[0], skip_special_tokens=True)
+        print(f"\nProcessed ImageCaptioning by BLIPCaptioner, Output Text: {captions}")
+        return captions
 
 
 if __name__ == '__main__':
---
+++ captioner/git.py
@@ -0,0 +1,34 @@
+from transformers import GitForCausalLM, GitProcessor, AutoProcessor
+from PIL import Image
+import torch
+from base_captioner import BaseCaptioner
+import numpy as np
+from typing import Union
+
+
+class GITCaptioner(BaseCaptioner):
+    def __init__(self, device):
+        super().__init__(device)
+        self.device = device
+        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
+        self.processor = AutoProcessor.from_pretrained("microsoft/git-large", cache_dir=self.huggingface_cache_dir)
+        self.model = GitForCausalLM.from_pretrained(
+            "microsoft/git-large", torch_dtype=self.torch_dtype, cache_dir=self.huggingface_cache_dir).to(self.device)
+        
+    def inference(self, image: Union[np.ndarray, Image.Image, str]):
+        if type(image) == str: # input path
+            image = Image.open(image)
+        pixel_values = self.processor(images=image, return_tensors="pt").pixel_values.to(self.device, self.torch_dtype)
+        generated_ids = self.model.generate(pixel_values=pixel_values, max_new_tokens=50)
+        generated_caption = self.processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        print(f"\nProcessed ImageCaptioning by GITCaptioner, Output Text: {generated_caption}")
+        return generated_caption
+
+
+if __name__ == '__main__':
+    model = GITCaptioner(device='cuda:0')
+    image_path = 'test_img/img2.jpg'
+    seg_mask = np.zeros((224,224))
+    seg_mask[50:200, 50:200] = 1
+    print(f'process image {image_path}')
+    print(model.inference_seg(image_path, seg_mask))
