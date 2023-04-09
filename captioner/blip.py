2180eb4c56c50ada16b24d5ee081d5106d3ced71
Ayaneru <953832303@qq.com>
2023-04-09 22:42:38 +0800

    cache

--- captioner/blip.py
+++ captioner/blip.py
@@ -12,13 +12,16 @@ import torchvision.transforms.functional as F
 
 
 class BLIPCaptioner(BaseCaptioner):
-    def __init__(self, device, enable_filter=False):
+    def __init__(self, device, enable_filter=False, cache_dir = None):
         super().__init__(device, enable_filter)
         self.device = device
         self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
-        self.processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-        self.model = BlipForConditionalGeneration.from_pretrained(
-            "Salesforce/blip-image-captioning-large", torch_dtype=self.torch_dtype).to(self.device)
+        if cache_dir is not None:
+            self.processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large", cache_dir = cache_dir)
+            self.model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large", cache_dir = cache_dir, torch_dtype=self.torch_dtype).to(self.device)
+        else:
+            self.processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+            self.model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large", torch_dtype=self.torch_dtype).to(self.device)
         
     @torch.no_grad()
     def inference(self, image: Union[np.ndarray, Image.Image, str]):
