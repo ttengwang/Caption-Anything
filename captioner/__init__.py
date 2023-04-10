6f4be33fef40d43ff3f23a64b30329464634dc1a
zjr <zhangjr200012@outlook.com>
2023-04-10 10:59:40 +0800

    Fix potential bugs

--- captioner/__init__.py
+++ captioner/__init__.py
@@ -6,10 +6,10 @@ from .base_captioner import BaseCaptioner
 
 def build_captioner(type, device, args=None):
     if type == 'blip':
-        return BLIPCaptioner(device, args.clip_filter)
+        return BLIPCaptioner(device, enable_filter=args.clip_filter)
     elif type == 'blip2':
-        return BLIP2Captioner(device, args.clip_filter)
+        return BLIP2Captioner(device, enable_filter=args.clip_filter)
     elif type == 'git':
-        return GITCaptioner(device, args.clip_filter)
+        return GITCaptioner(device, enable_filter=args.clip_filter)
     else:
         raise NotImplementedError("")
