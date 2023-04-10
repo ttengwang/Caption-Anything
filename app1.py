297acd849687a717fde933df84b2cb2501e01a4b
zjr <zhangjr200012@outlook.com>
2023-04-10 16:12:44 +0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -9,10 +9,11 @@ import sys
 import argparse
 from caas import parse_augment
 import numpy as np
+import PIL.ImageDraw as ImageDraw
 
 title = """<h1 align="center">Caption-Anything</h1>"""
 description = """Gradio demo for Caption Anything, image to dense captioning generation with various language styles. To use it, simply upload your image, or click one of the examples to load them.
-<br> <strong>Code</strong>: GitHub repo: <a href='https://github.com/ttengwang/Caption-Anything' target='_blank'></a>
+<br> <strong>Code</strong>: GitHub repo: < a href=' ' target='_blank'></ a>
 """
 
 examples = [
@@ -20,18 +21,12 @@ examples = [
 ]
 
 args = parse_augment()
-
-# args = type('args', (object,), {})()
-# args.captioner='blip'
-# args.segmenter='base'
-# args.text_refiner = 'base'
-# args.segmenter_checkpoint = 'segmenter/sam_vit_h_4b8939.pth'
-# args.seg_crop_mode = 'w_bg'
-# args.clip_filter = False
-# args.device = "cuda" if torch.cuda.is_available() else "cpu"
+args.device = 'cuda:1'
+args.disable_gpt = True
+args.enable_reduce_tokens = True
+args.port=20321
 
 model = CaptionAnything(args)
-# model = None
 
 def get_prompt(chat_input, click_state):    
     points = click_state[0]
@@ -49,35 +44,29 @@ def get_prompt(chat_input, click_state):
     }
     return prompt
     
-def inference_seg_cap(image_input, chat_input, language, sentiment, factuality, length, state, click_state):
+def inference_seg_cap(image_input, chat_input, language, sentiment, factuality, length, state, click_state, evt:gr.SelectData):
     controls = {'length': length,
              'sentiment': sentiment,
              'factuality': factuality,
              'language': language}
+    click_coordinate = "[[{}, {}, 1]]".format(str(evt.index[0]), str(evt.index[1])) 
+    chat_input = click_coordinate
     prompt = get_prompt(chat_input, click_state)
     print('prompt: ', prompt, 'controls: ', controls)
     out = model.inference(image_input, prompt, controls)
-    chatbot = state + [
-        ('raw caption: ' + out['generated_captions']['raw_caption'], None), 
-        ('caption: ' + out['generated_captions']['caption'], None), 
-        ('wiki:' + out['generated_captions']['wiki'], None)
-        ]
-    image_output_mask = out['mask_save_path']
-    image_output_crop = out['crop_save_path']
-    return chatbot, chatbot, click_state, image_output_mask, image_output_crop
+    state = state + [(None, "Image point: {}, Input label: {}".format(prompt["input_point"], prompt["input_label"]))]
+    for k, v in out['generated_captions'].items():
+        state = state + [(f'{k}: {v}', None)]
+    
+    text = out['generated_captions']
+    draw = ImageDraw.Draw(image_input)
+    draw.text((evt.index[0], evt.index[1]), text, textcolor=(0,0,255), text_size=120) 
+    return text, click_state, chat_input, image_input
 
 
 def upload_callback(image_input, state):
     state = state + [('Image size: ' + str(image_input.size), None)]
     return state
-
-# get coordinate in format [[x,y,positive/negative]]
-def get_select_coords(image_input: gr.Image, evt: gr.SelectData):
-
-        coordinate = "[[{}, {}, 1]]".format(str(evt.index[0]), str(evt.index[1]))
-        out = image_input.copy() * 0.2
-        out = out.astype(np.uint8)
-        return coordinate, out
 
 
 with gr.Blocks(
@@ -94,9 +83,8 @@ with gr.Blocks(
 
     with gr.Row():
         with gr.Column(scale=0.7):
-            image_input = gr.Image(type="pil", interactive=True, onchange=upload_callback).style(height=260,scale=1.0)
+            image_input = gr.Image(type="pil", interactive=True, onchange=upload_callback).style(height=680,width=1280, scale=1.0)
 
-            # with gr.Row():
             language = gr.Radio(
                 choices=["English", "Chinese", "French", "Spanish", "Arabic", "Portuguese","Cantonese"],
                 value="English",
@@ -124,80 +112,91 @@ with gr.Blocks(
                 label="Length",
             )
 
-        with gr.Column(scale=1.5):
+    with gr.Row(scale=1.5):
+        with gr.Row():
+            image_output_mask= gr.Image(type="pil", interactive=False).style(height=260,scale=1.0)
+            image_output_crop= gr.Image(type="pil", interactive=False).style(height=260,scale=1.0)
+        # chatbot = gr.Chatbot(label="Chat Output",).style(height=500,scale=0.5)
+        with gr.Column():
+        # with gr.Column(scale=1):
+            chat_input = gr.Textbox(lines=1, label="Chat Input")
+            # chat_input.submit(
+            #     inference_seg_cap,
+            #     [
+            #         image_input,
+            #         chat_input,
+            #         language,
+            #         sentiment,
+            #         factuality,
+            #         length,
+            #         state,
+            #         click_state
+            #     ],
+            #     [state, click_state, image_output_mask, image_output_crop],
+            # )
+            
+            image_input.upload(
+                upload_callback,
+                [image_input, state],
+                [state]
+                )
+
             with gr.Row():
-                image_output_mask= gr.Image(type="pil", interactive=False).style(height=260,scale=1.0)
-                image_output_crop= gr.Image(type="pil", interactive=False).style(height=260,scale=1.0)
-            chatbot = gr.Chatbot(label="Chat Output",).style(height=500,scale=0.5)
-            with gr.Row():
-            # with gr.Column(scale=1):
-                chat_input = gr.Textbox(lines=1, label="Chat Input")
-                chat_input.submit(
-                    inference_seg_cap,
-                    [
-                        image_input,
-                        chat_input,
-                        language,
-                        sentiment,
-                        factuality,
-                        length,
-                        state,
-                        click_state
-                    ],
-                    [chatbot, state, click_state, image_output_mask, image_output_crop],
+                clear_button = gr.Button(value="Clear Click", interactive=True)
+                clear_button.click(
+                    lambda: ("", [[], []], [], []),
+                    [],
+                    [chat_input, click_state, image_output_mask, image_output_crop],
+                    queue=False,
                 )
                 
-                image_input.upload(
-                    upload_callback,
-                    [image_input, state],
-                    [chatbot]
-                    )
+                clear_button = gr.Button(value="Clear", interactive=True)
+                clear_button.click(
+                    lambda: ("", [], [], [[], []], [], []),
+                    [],
+                    [chat_input, state, click_state, image_output_mask, image_output_crop],
+                    queue=False,
+                )
 
-                with gr.Row():
-                    clear_button = gr.Button(value="Clear Click", interactive=True)
-                    clear_button.click(
-                        lambda: ("", [[], []], [], []),
-                        [],
-                        [chat_input, click_state, image_output_mask, image_output_crop],
-                        queue=False,
-                    )
-                    
-                    clear_button = gr.Button(value="Clear", interactive=True)
-                    clear_button.click(
-                        lambda: ("", [], [], [[], []], [], []),
-                        [],
-                        [chat_input, chatbot, state, click_state, image_output_mask, image_output_crop],
-                        queue=False,
-                    )
+                # submit_button = gr.Button(
+                #     value="Submit", interactive=True, variant="primary"
+                # )
+                # submit_button.click(
+                #     inference_seg_cap,
+                #     [
+                #         image_input,
+                #         chat_input,
+                #         language,
+                #         sentiment,
+                #         factuality,
+                #         length,
+                #         state,
+                #         click_state
+                #     ],
+                #     [state, click_state, image_output_mask, image_output_crop],
+                # )
 
-                    submit_button = gr.Button(
-                        value="Submit", interactive=True, variant="primary"
-                    )
-                    submit_button.click(
-                        inference_seg_cap,
-                        [
-                            image_input,
-                            chat_input,
-                            language,
-                            sentiment,
-                            factuality,
-                            length,
-                            state,
-                            click_state
-                        ],
-                        [chatbot, state, click_state, image_output_mask, image_output_crop],
-                    )
+        # select coordinate
+        image_input.select(inference_seg_cap, 
+                           inputs=[
+                                    image_input,
+                                    chat_input,
+                                    language,
+                                    sentiment,
+                                    factuality,
+                                    length,
+                                    state,
+                                    click_state
+                                    ],
+                            outputs=[state, click_state, chat_input, image_input])
+    
 
-            # select coordinate
-            image_input.select(get_select_coords, inputs=image_input,outputs=[chat_input, image_input])
-        
-
-            image_input.change(
-                lambda: ("", [], [[], []]),
-                [],
-                [chatbot, state, click_state],
-                queue=False,
-            )
+        image_input.change(
+            lambda: ([], [[], []]),
+            [],
+            [state, click_state],
+            queue=False,
+        )
     examples = gr.Examples(
         examples=examples,
         inputs=[image_input, chat_input],
