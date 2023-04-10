ec640ecc5a92b108e71a4c9f4600cf9af39407a9
memoryunreal <814514103@qq.com>
2023-04-10 17:38:16 +0000

    requirements.txt upload

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 conda create -n caption python=3.9 -y 
 source activate caption
-pip install -r requirement.txt 
+pip install -r requirements.txt 
 cd segmenter
 wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth 
 
