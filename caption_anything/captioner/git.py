8049fab730927ccd3055a0344401d4823868a465
zh-plus <zhenghaosustc@gmail.com>
2023-04-23 18:21:10 +0800

    Replace all manually load_image code into function.

--- caption_anything/captioner/git.py
+++ caption_anything/captioner/git.py
@@ -1,4 +1,6 @@
 from transformers import GitProcessor, AutoProcessor
+
+from caption_anything.utils.utils import load_image
 from .modeling_git import GitForCausalLM
 from PIL import Image
 import torch
@@ -15,11 +17,10 @@ class GITCaptioner(BaseCaptioner):
         self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
         self.processor = AutoProcessor.from_pretrained("microsoft/git-large")
         self.model = GitForCausalLM.from_pretrained("microsoft/git-large", torch_dtype=self.torch_dtype).to(self.device)
-    
+
     @torch.no_grad()
     def inference(self, image: Union[np.ndarray, Image.Image, str], filter=False):
-        if type(image) == str: # input path
-            image = Image.open(image)
+        image = load_image(image, return_type="pil")
         pixel_values = self.processor(images=image, return_tensors="pt").pixel_values.to(self.device, self.torch_dtype)
         generated_ids = self.model.generate(pixel_values=pixel_values, max_new_tokens=50)
         generated_caption = self.processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
@@ -29,10 +30,11 @@ class GITCaptioner(BaseCaptioner):
         return generated_caption
 
     @torch.no_grad()
-    def inference_with_reduced_tokens(self, image: Union[np.ndarray, Image.Image, str], seg_mask, crop_mode="w_bg", filter=False, disable_regular_box = False):
-        crop_save_path = self.generate_seg_cropped_image(image=image, seg_mask=seg_mask, crop_mode=crop_mode, disable_regular_box=disable_regular_box)
-        if type(image) == str: # input path
-            image = Image.open(image)
+    def inference_with_reduced_tokens(self, image: Union[np.ndarray, Image.Image, str], seg_mask, crop_mode="w_bg",
+                                      filter=False, disable_regular_box=False):
+        crop_save_path = self.generate_seg_cropped_image(image=image, seg_mask=seg_mask, crop_mode=crop_mode,
+                                                         disable_regular_box=disable_regular_box)
+        image = load_image(image, return_type="pil")
         inputs = self.processor(images=image, return_tensors="pt")
         pixel_values = inputs.pixel_values.to(self.device, self.torch_dtype)
         _, _, H, W = pixel_values.shape
@@ -48,10 +50,11 @@ class GITCaptioner(BaseCaptioner):
         print(f"\nProcessed ImageCaptioning by BLIPCaptioner, Output Text: {captions}")
         return captions, crop_save_path
 
+
 if __name__ == '__main__':
     model = GITCaptioner(device='cuda:2', enable_filter=False)
     image_path = 'test_images/img2.jpg'
-    seg_mask = np.zeros((224,224))
+    seg_mask = np.zeros((224, 224))
     seg_mask[50:200, 50:200] = 1
     print(f'process image {image_path}')
     print(model.inference_with_reduced_tokens(image_path, seg_mask))
